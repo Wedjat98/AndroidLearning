913b9be3fbf8b636a204c851f92fbe771f23c190
Administrator <11>
2022-04-30 18:01:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/javademoapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.javademoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("AdminMSG","onCreate");
+
+
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,subMainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
