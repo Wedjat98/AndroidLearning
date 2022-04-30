f37a2fc9d2c5bfebecccd0b7118cc60d31a96d09
Administrator <11>
2022-04-30 22:39:19 +0900

    init

--- app/src/main/java/com/example/javademoapp/subMainActivity3.java
+++ app/src/main/java/com/example/javademoapp/subMainActivity3.java
@@ -1,11 +1,10 @@
 package com.example.javademoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class subMainActivity3 extends AppCompatActivity {
 
@@ -15,13 +14,10 @@ public class subMainActivity3 extends AppCompatActivity {
         setContentView(R.layout.activity_sub_main3);
         Button button = findViewById(R.id.btn);
         button.setText("四");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClass(subMainActivity3.this,subMainActivity3.class);
-                startActivity(intent);
-            }
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setClass(subMainActivity3.this,subMainActivity3.class);
+            startActivity(intent);
         });
     }
 }
