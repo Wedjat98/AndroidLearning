b78e9b3c1cd89e83dc1527dc3c7280b7e684254e
Administrator <11>
2022-05-01 00:14:59 +0900

    init

--- app/src/main/java/com/example/javademoapp/subMainActivity6.java
+++ app/src/main/java/com/example/javademoapp/subMainActivity6.java
@@ -6,17 +6,17 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class subMainActivity4 extends AppCompatActivity {
+public class subMainActivity6 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_submain4);
+        setContentView(R.layout.activity_submain6);
         Button button = findViewById(R.id.btn);
         button.setText("点击这里");
         button.setOnClickListener(view -> {
             Intent intent = new Intent();
-            intent.setClass(subMainActivity4.this,subMainActivity5.class);
+            intent.setClass(subMainActivity6.this, subMainActivity6.class);
             startActivity(intent);
         });
     }
